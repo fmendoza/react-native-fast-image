d97e9fb0cee4e450b2379c092e0ae8e33e26624c
Fernando <f.mendoza.galvez@gmail.com>
2022-08-15 17:26:23 -0500

    fix(android): allow background processing

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewModule.java
@@ -1,6 +1,7 @@
 package com.dylanvann.fastimage;
 
 import android.app.Activity;
+import android.content.Context;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.model.GlideUrl;
@@ -10,13 +11,11 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.views.imagehelper.ImageSource;
 import androidx.annotation.Nullable;
 import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
-import com.facebook.react.bridge.Promise;
 import java.io.File;
 
 class FastImageViewModule extends ReactContextBaseJavaModule {
@@ -24,9 +23,11 @@ class FastImageViewModule extends ReactContextBaseJavaModule {
     private static final String REACT_CLASS = "FastImagePreloaderManager";
     private static final String ERROR_LOAD_FAILED = "ERROR_LOAD_FAILED";
     private int preloaders = 0;
+    private Context mContext;
 
     FastImageViewModule(ReactApplicationContext reactContext) {
         super(reactContext);
+        this.mContext = reactContext;
     }
 
     @Override
@@ -42,34 +43,18 @@ class FastImageViewModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void preload(final int preloaderId, final ReadableArray sources) {
         final Activity activity = getCurrentActivity();
-        if (activity == null) return;
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                FastImagePreloaderListener preloader = new FastImagePreloaderListener(getReactApplicationContext(), preloaderId, sources.size());
-                for (int i = 0; i < sources.size(); i++) {
-                    final ReadableMap source = sources.getMap(i);
-                    final FastImageSource imageSource = FastImageViewConverter.getImageSource(activity, source);
 
-                    Glide
-                            .with(activity.getApplicationContext())
-                            .downloadOnly()
-                            // This will make this work for remote and local images. e.g.
-                            //    - file:///
-                            //    - content://
-                            //    - res:/
-                            //    - android.resource://
-                            //    - data:image/png;base64
-                            .load(
-                                    imageSource.isBase64Resource() ? imageSource.getSource() :
-                                    imageSource.isResource() ? imageSource.getUri() : imageSource.getGlideUrl()
-                            )
-                            .listener(preloader)
-                            .apply(FastImageViewConverter.getOptions(activity, imageSource, source))
-                            .preload();
+        if (activity != null) {
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    runPreload(preloaderId, sources);
                 }
-            }
-        });
+            });
+        } else {
+            runPreload(preloaderId, sources);
+        }
+
     }
 
     @ReactMethod
@@ -104,38 +89,73 @@ class FastImageViewModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void getCachePath(final ReadableMap source, final Promise promise) {
         final Activity activity = getCurrentActivity();
-        if (activity == null) return;
 
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                final FastImageSource imageSource = FastImageViewConverter.getImageSource(activity, source);
-                final GlideUrl glideUrl = imageSource.getGlideUrl();
-
-                if (glideUrl == null) {
-                    promise.resolve(null);
-                    return;
+        if (activity != null) {
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    runGetCachePath(source, promise);
                 }
+            });
+        } else {
+            runGetCachePath(source, promise);
+        }
 
-                Glide
-                        .with(activity.getApplicationContext())
-                        .asFile()
-                        .load(glideUrl)
-                        .apply(FastImageViewConverter.getOptions(activity, imageSource,  source))
-                        .listener(new RequestListener<File>() {
-                            @Override
-                            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<File> target, boolean isFirstResource) {
-                                promise.reject(ERROR_LOAD_FAILED, e);
-                                return false;
-                            }
-                            @Override
-                            public boolean onResourceReady(File resource, Object model, Target<File> target, DataSource dataSource, boolean isFirstResource) {
-                                promise.resolve(resource.getAbsolutePath());
-                                return false;
-                            }
-                        })
-                        .submit();
-            }
-        });
     }
+
+    private void runPreload(final int preloaderId, final ReadableArray sources) {
+        FastImagePreloaderListener preloader = new FastImagePreloaderListener(getReactApplicationContext(), preloaderId, sources.size());
+        for (int i = 0; i < sources.size(); i++) {
+            final ReadableMap source = sources.getMap(i);
+            final FastImageSource imageSource = FastImageViewConverter.getImageSource(mContext, source);
+
+            Glide
+                    .with(mContext)
+                    .downloadOnly()
+                    // This will make this work for remote and local images. e.g.
+                    //    - file:///
+                    //    - content://
+                    //    - res:/
+                    //    - android.resource://
+                    //    - data:image/png;base64
+                    .load(
+                            imageSource.isBase64Resource() ? imageSource.getSource() :
+                                    imageSource.isResource() ? imageSource.getUri() : imageSource.getGlideUrl()
+                    )
+                    .listener(preloader)
+                    .apply(FastImageViewConverter.getOptions(mContext, imageSource, source))
+                    .preload();
+        }
+    }
+
+    private void runGetCachePath(final ReadableMap source, final Promise promise) {
+        final FastImageSource imageSource = FastImageViewConverter.getImageSource(mContext, source);
+        final GlideUrl glideUrl = imageSource.getGlideUrl();
+
+        if (glideUrl == null) {
+            promise.resolve(null);
+            return;
+        }
+
+        Glide
+                .with(mContext)
+                .asFile()
+                .load(glideUrl)
+                .apply(FastImageViewConverter.getOptions(mContext, imageSource,  source))
+                .listener(new RequestListener<File>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<File> target, boolean isFirstResource) {
+                        promise.reject(ERROR_LOAD_FAILED, e);
+                        return false;
+                    }
+                    @Override
+                    public boolean onResourceReady(File resource, Object model, Target<File> target, DataSource dataSource, boolean isFirstResource) {
+                        promise.resolve(resource.getAbsolutePath());
+                        return false;
+                    }
+                })
+                .submit();
+    }
+
+
 }
